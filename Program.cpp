1ea052c25ae223dd2128e7d9a7466fdd746775bf
Thatipamula Sasi Kiran <sasitsk0804@gmail.com>
2026-06-17 22:07:24 +0530

    Basic User Input

--- Program.cpp
+++ Program.cpp
@@ -2,8 +2,20 @@
 #include <string>
 
 int main() {
+    std::string message;
+    int ShiftValue;
+
     std::cout << "Hello Human!" << std::endl;
     std::cout << "This is a program for Encrypting and Decrypting messages using the Caesar Cipher." << std::endl;
+
+    std::cout << "Enter the message you would like to encrypt or decrypt: ";
+    std::getline(std::cin, message);
+
+    std::cout << "Enter the shift value:";
+    std::cin >> ShiftValue;
+
+    std::cout << "Message: " << message << std::endl;
+    std::cout << "Shift Value: " << ShiftValue << std::endl;
     
     return 0;
 }
