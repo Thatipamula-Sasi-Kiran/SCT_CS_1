4d6b6422adea8ac85afd77b58888fdb67c6a5102
Thatipamula Sasi Kiran <sasitsk0804@gmail.com>
2026-06-25 21:14:56 +0530

    Adding Modularity

--- Program.cpp
+++ Program.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <string>
 
+int encryptMessage (std::string message, int Shiftvalue)
+{
+    
+}
+
+int decryptMessage (std::string message, int Shiftvalue)
+{
+    
+}
+
 int main() {
     std::string message;
     int ShiftValue;
@@ -8,14 +18,28 @@ int main() {
     std::cout << "Hello Human!" << std::endl;
     std::cout << "This is a program for Encrypting and Decrypting messages using the Caesar Cipher." << std::endl;
 
+    std::cout << "Do you want to encrypt or decrypt a message? (Enter 'e' or 'd'): ";
+    char choice;
+    std::cin >> choice;
+
     std::cout << "Enter the message you would like to encrypt or decrypt: ";
     std::getline(std::cin, message);
 
     std::cout << "Enter the shift value:";
     std::cin >> ShiftValue;
 
+    if (choice == 'e' || choice == 'E')
+    {
+        encryptMessage(message, ShiftValue);
+    }
+    else if (choice == 'd' || choice == 'D')
+    {
+        decryptMessage(message, ShiftValue);
+    }
+
     std::cout << "Message: " << message << std::endl;
     std::cout << "Shift Value: " << ShiftValue << std::endl;
     
     return 0;
 }
+
